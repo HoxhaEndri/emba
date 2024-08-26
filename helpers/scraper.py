fa12fd5f70349122292543338c179ab021013ad2
kali <kali@kali>
2024-08-26 14:38:53 +0200

    snyk in python

---
+++ helpers/scraper.py
@@ -0,0 +1,79 @@
+import os
+import re
+import sys
+import requests
+
+base_url = "https://security.snyk.io/vuln/"
+
+def extract_links(results):
+    ret=[]
+    for link in results:
+        ret.append(base_url+link["href"][5:])
+    return ret
+    
+def generate_links_file(list):
+    with open('links.txt', 'w') as f:
+        for line in list:
+            f.write(f"{line}\n")
+
+def bool_to_str(bool):
+    if bool:
+        return "yes"
+    else:
+        return "no"
+
+def name_from_url(url):
+    return url.split("vuln/")[1][:-1]
+            
+def check_snyk(vuln_url):
+    ret = ""
+    PoC=False
+    Github=False
+    Curl=False
+    Xml=False
+    cves = list()
+    try:
+        page = requests.get(vuln_url)
+        #https://www.cve.org/CVERecord?id=CVE-2024-5585
+        cves = list(set(re.findall("id\=CVE-[0-9]{4}-[0-9]+",page.content.decode())))
+
+        if len(cves) > 0:
+            x = re.search("PoC",page.content.decode())
+    
+            if x != None:
+                PoC= True
+                tmp = re.search("GitHub PoC", page.content.decode())
+
+                if tmp != None:
+                    Github=True
+
+                tmp = re.search("curl http", page.content.decode())
+
+                if tmp != None:
+                    Curl=True
+
+                tmp = re.search("For example the below code contains", page.content.decode())
+
+                if tmp != None:
+                    Xml=True
+
+                for cve in cves:
+                    ret += cve[3:] + ";" + name_from_url(vuln_url) + ";" + vuln_url[:-1] + ";" + bool_to_str(PoC) + ";" + bool_to_str(Github) + ";" + bool_to_str(Curl) + ";" + bool_to_str(Xml) + ";" + "\n"
+    except ConnectionError:
+        print("Failed to open this url: "+vuln_url)
+    return ret
+
+def main():
+
+    result = "CVE;advisory name;advisory URL;unknown PoC;Github PoC;Curl PoC;XML PoC;\n"
+    with open(sys.argv[1]) as links:
+        for link in links:
+            temp = check_snyk(link)
+            result += temp
+
+    with open(sys.argv[2], "w") as filetowrite:
+        filetowrite.write(result)
+
+
+if __name__ == "__main__":
+    main()
--- helpers/snyk_crawler.sh
+++ helpers/snyk_crawler.sh
@@ -75,95 +75,16 @@ else
 fi
 
 # remove the numbering at the beginning of every entry:
-sed 's/.*http/http/' "${SAVE_PATH}"/"${LINKS}" | sort -u > "${SAVE_PATH}"/"${LINKS}"_sorted
+tail -n +2 "${SAVE_PATH}"/"${LINKS}" | sed 's/.*http/http/' | sort -u > "${SAVE_PATH}"/"${LINKS}"_sorted
 
 ADV_CNT="$(wc -l "${SAVE_PATH}"/"${LINKS}"_sorted | awk '{print $1}')"
 echo -e "[*] Detected ${ORANGE}${ADV_CNT}${NC} advisories for download"
 echo ""
 
-ID=1
-while read -r ADV; do
-  ((ID+=1))
-  FILENAME="$(echo "${ADV}" | rev | cut -d '/' -f1 | rev)"
-  if [[ -f "${SAVE_PATH}/vuln/${FILENAME}" ]]; then
-    echo -e "[-] Already downloaded ${ORANGE}${FILENAME}${NC}"
-    continue
-  fi
-  echo -e "[*] Downloading ${ORANGE}${FILENAME}${NC} (${ORANGE}${ID}${NC}/${ORANGE}${ADV_CNT}${NC}) to ${ORANGE}${SAVE_PATH}/vuln/${FILENAME}${NC}"
-  lynx -useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.79 Safari/537.1" -dump -hiddenlinks=listonly "${ADV}" > "${SAVE_PATH}"/vuln/"${FILENAME}"
-done < "${SAVE_PATH}"/"${LINKS}"_sorted
+#tail -n+2 "${SAVE_PATH}"/"${LINKS}"_sorted | head -100  > "${SAVE_PATH}"/"${LINKS}"_sorted1
 
-echo -e "[*] Finished downloading ${ORANGE}${ADV_CNT}${NC} advisories to ${ORANGE}${SAVE_PATH}/vuln${NC}"
-echo ""
-
-echo -e "[*] The following advisories have PoC code included:"
-PoC_CNT=0
-echo "CVE;advisory name;advisory URL;unknown PoC;Github PoC;Curl PoC;XML PoC;" > "${SAVE_PATH}"/Snyk_PoC_results.csv
-
-while IFS= read -r -d '' ADV; do
-  PoC_PoC="no"
-  PoC_GH="no"
-  PoC_EDB="no"
-  PoC_CURL="no"
-  PoC_XML="no"
-  CVE="NA"
-  ADV_NAME=$(basename "${ADV}")
-  ADV_URL="${URL}"/"${ADV_NAME}"
-
-  # unsure if this is good enough:
-  PoC_PoC=$(grep -c -a "PoC" "${ADV}")
-  PoC="${PoC_PoC}"
-  if [[ "${PoC_PoC}" -gt 0 ]]; then
-    PoC_PoC="yes"
-  else
-    PoC_PoC="no"
-  fi
-  # GitHub PoC references:
-  PoC_GH=$(grep -a -c "GitHub PoC" "${ADV}")
-  ((PoC+="${PoC_GH}"))
-  if [[ "${PoC_GH}" -gt 0 ]]; then
-    PoC_GH="yes"
-  else
-    PoC_GH="no"
-  fi
-  # removed exploit-db as we already have it in EMBA
-  # exploit-db references:
-  # PoC_EDB=$(grep -a -c -E "https://www.exploit-db.com/exploits/[0-9]+" "${ADV}")
-  # ((PoC+="$PoC_EDB"))
-  # if [[ "$PoC_EDB" -gt 0 ]]; then
-  #  PoC_EDB="yes"
-  # else
-  #  PoC_EDB="no"
-  # fi
-  # curl http exploits:
-  PoC_CURL=$(grep -a -c "curl http" "${ADV}")
-  ((PoC+="${PoC_CURL}"))
-  if [[ "${PoC_CURL}" -gt 0 ]]; then
-    PoC_CURL="yes"
-  else
-    PoC_CURL="no"
-  fi
-  # xml exploits:
-  PoC_XML=$(grep -a -c "For example the below code contains" "${ADV}")
-  ((PoC+="${PoC_XML}"))
-  if [[ "${PoC_XML}" -gt 0 ]]; then
-    PoC_XML="yes"
-  else
-    PoC_XML="no"
-  fi
-  mapfile -t CVEs < <(grep -a -o -E "CVE-[0-9]{4}-[0-9]+" "${ADV}" | sort -u)
-
-  if [[ "${PoC}" -gt 0 ]] && [[ "${#CVEs[@]}" -gt 0 ]]; then
-    for CVE in "${CVEs[@]}"; do
-      echo -e "[+] Found PoC for ${ORANGE}${CVE}${NC} in advisory ${ORANGE}${ADV_NAME}${NC} (unknown PoC: ${ORANGE}${PoC_PoC}${NC} / Github: ${ORANGE}${PoC_GH}${NC} / exploit-db: ${ORANGE}${PoC_EDB}${NC} / Curl: ${ORANGE}${PoC_CURL}${NC} / XML: ${ORANGE}${PoC_XML}${NC})"
-      echo "${CVE};${ADV_NAME};${ADV_URL};${PoC_PoC};${PoC_GH};${PoC_CURL};${PoC_XML};" >> "${SAVE_PATH}"/Snyk_PoC_results.csv
-      ((PoC_CNT+=1))
-    done
-  fi
-done < <(find "${SAVE_PATH}"/vuln/ -type f -print0)
-
+python scraper.py "${SAVE_PATH}"/"${LINKS}"_sorted "${SAVE_PATH}"/Snyk_PoC_results.csv
 sort -nr -o "${SAVE_PATH}"/Snyk_PoC_results.csv "${SAVE_PATH}"/Snyk_PoC_results.csv
-
 
 if [[ -f "${SAVE_PATH}"/Snyk_PoC_results.csv ]] && [[ -d "${EMBA_CONFIG_PATH}" ]]; then
   uniq "${SAVE_PATH}"/Snyk_PoC_results.csv > "${EMBA_CONFIG_PATH}"/Snyk_PoC_results.csv
@@ -172,7 +93,3 @@ if [[ -f "${SAVE_PATH}"/Snyk_PoC_results.csv ]] && [[ -d "${EMBA_CONFIG_PATH}" ]
 else
   echo "[-] Not able to copy generated PoC file to configuration directory ${EMBA_CONFIG_PATH}"
 fi
-
-echo -e "${GREEN}[+] Found ${ORANGE}${PoC_CNT_BEFORE}${GREEN} advisories with PoC code (before udpate)."
-echo ""
-echo -e "${GREEN}[+] Found ${ORANGE}${PoC_CNT}${GREEN} advisories with PoC code (after update)."
